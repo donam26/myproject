89ad433a0d4c080ef12924f92063c817d30eae9c
Do Nam <abcnam26@gmail.com>
2024-11-06 18:12:33 +0700

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -107,7 +107,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -123,3 +122,5 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = '/' 
 
 LOGIN_URL = '/login/'  
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- myproject/urls.py
+++ myproject/urls.py
@@ -3,11 +3,19 @@ from django.urls import path
 from home import views as home
 from admin_custom import views as admin_custom
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('admin_dashboard/', admin_custom.admin_dashboard, name='admin_dashboard'), 
-
+    path('admin_dashboard/customers', admin_custom.admin_customer), 
+    path('admin_dashboard/employees', admin_custom.admin_employee), 
+    path('admin_dashboard/rooms', admin_custom.admin_room, name='admin_room'), 
+    path('admin_dashboard/services', admin_custom.admin_service), 
+    path('admin_dashboard/posts', admin_custom.admin_post), 
+    path('room/edit/<int:room_id>/', admin_custom.edit_room, name='edit_room'),
+    path('room/delete/<int:room_id>/', admin_custom.delete_room, name='delete_room'),
 
     path('', home.get_home, name='home'),
     path('contact', home.get_contact),
@@ -16,9 +24,10 @@ urlpatterns = [
     path('room', home.get_room),
     path('room/<int:room_id>/', home.room_detail, name='room_detail'),  
 
-
     path('login/', home.user_login, name='login'),
     path('register/', home.user_register, name='register'),
-
-
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
